f105deb55c78084f344e1e9dcf544d5a8b130e0c
Michele Freitas <michele-braga@hotmail.com>
2019-08-03 16:54:46 +0100

    insercao com recuperacao do id inserido no BD

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -26,7 +28,8 @@ public class Program {
 			st = conn.prepareStatement("INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)");
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);//inserção com recuperação do id inserido.Acrescenta valor após String total
 			
 			//comandos para trocar as "?" pelo que definir. O 1 corresponde ao primeiro "?", nome.
 			st.setString(1, "Carl Purple");
@@ -40,8 +43,20 @@ public class Program {
 			//o resultado da operação é um número inteiro indicando quantas linhas foram alteradas no BD
 			int rowsAffected = st.executeUpdate();
 			
-			//pronto
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			//caso alguma linha alterada 
+			if(rowsAffected >0) {
+				ResultSet rs = st.getGeneratedKeys();//pode retornar rs com um ou mais valores
+				
+				while(rs.next()) {
+					//O 1 indica valor da primeira coluna
+					int id = rs.getInt(1);//tab. auxiliar que terá só uma coluna contendo os id
+					
+					System.out.println("Done! Id= " + id);
+				}
+				
+			}else {
+				System.out.println("No row affected!");
+			}
 			
 		}catch(SQLException e) {
 			e.printStackTrace();
